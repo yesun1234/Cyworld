c31c946e3c5b6c5556d34b56a7fb522ec14f68bf
yesun1234 <dptjs1106@naver.com>
2024-10-23 23:21:52 +0900

    home

--- src/nav/Home.jsx
+++ src/nav/Home.jsx
@@ -1,26 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Img from '../image/miniroom.jpeg'
+import { Link } from 'react-router-dom'
+import { PiArrowElbowDownRightBold } from "react-icons/pi";
 const Home = () => {
+  const [inputVisible, setInputVisble] = useState(false)
+  const handleCommentClick = () => {
+    setInputVisble(!inputVisible);
+  }
   return (
     <div className='home'>
       <div className='homeTop'>
         <p>최근 게시물</p>
         <ul>
-          <li><span>동영상</span></li>
-          <li><span>게시판</span></li>
-          <li><span>스킨, 메뉴효과 예약기능</span></li>
-          <li><span>더욱 편리해진 사용중 아이템</span></li>
+          <Link to=''><li><span>동영상</span><span>맘에 드는 아이템을 소망상...</span></li></Link>
+          <Link to=''><li><span>게시판</span><span>내 개성은 미니미로</span></li></Link>
+          <Link to=''><li><span>갤러리</span><span>스킨, 메뉴효과 예약기능</span></li></Link>
+          <Link to=''><li><span>사진첩</span><span>더욱 편리해진 사용중 아이템</span></li></Link>
         </ul>
         <ul>
-          <li>방명록 22/225 <span>n</span></li>
-          <li>사진첩 <span>n</span></li>
+          <Link to=''><li>방명록 <span>0/3</span><span>n</span></li></Link>
+          <Link to=''><li>사진첩 <span>0/3</span><span>n</span></li></Link>
+          <li></li>
+          <li></li>
         </ul>
         <ul>
-          <li>즐겨찾기 <span>0/3</span></li>
-          <li>다이어리 <span>0/12</span></li>
+          <Link to=''><li>즐겨찾기 <span>0/3</span><span>n</span></li></Link>
+          <Link to=''><li>다이어리 <span>0/12</span><span>n</span></li></Link>
+        <li></li>
+        <li></li>
         </ul>
       </div>
       <div className='homeMain'>
+        <p>싸이월드</p>
         <img src={Img} alt="미니룸" />
       </div>
       <div className='homeBottom'>
@@ -31,10 +42,42 @@ const Home = () => {
           <span><button>확인</button></span>
         </div>
         <ul>
-          <li>ㅎㅇ <span>(친구 <span>노제</span>) <span>2009.10.11</span> </span></li>
-          <li>ㄲㅈ <span>(친구 <span>노제</span>) <span>2009.10.11</span></span></li>
-          <li>ㅋㅋ <span>(친구 <span>노제</span>) <span>2009.10.11</span></span></li>
-          <li>ㅊㅊ <span>(친구 <span>노제</span>) <span>2009.10.11</span></span></li>
+          <li>
+            <span>ㅎㅇ</span> <span>(친구 <span className='friendId'>노제</span>) <span className='friendDate'>2009.10.11</span> </span> <button className='friendComment' onClick={handleCommentClick}><PiArrowElbowDownRightBold /></button>
+            {inputVisible && (
+                <div>
+                  <input type="text" placeholder='댓글을 남겨보세요' />
+                  <button>확인</button>
+                </div>
+              )}
+          </li>
+          <li>
+            <span>ㅎㅇ</span> <span>(친구 <span className='friendId'>노제</span>) <span className='friendDate'>2009.10.11</span></span>  <button className='friendComment' onClick={handleCommentClick}><PiArrowElbowDownRightBold /></button>
+            {inputVisible && (
+                <div>
+                  <input type="text" placeholder='댓글을 남겨보세요' />
+                  <button>확인</button>
+                </div>
+              )}
+          </li>
+          <li>
+            <span>ㅎㅇ</span> <span>(친구 <span className='friendId'>노제</span>) <span className='friendDate'>2009.10.11</span></span> <button className='friendComment' onClick={handleCommentClick}><PiArrowElbowDownRightBold /></button>
+            {inputVisible && (
+                <div>
+                  <input type="text" placeholder='댓글을 남겨보세요' />
+                  <button>확인</button>
+                </div>
+              )}
+          </li>
+          <li>
+            <span>ㅎㅇ</span> <span>(친구 <span className='friendId'>노제</span>) <span className='friendDate'>2009.10.11</span></span> <button className='friendComment' onClick={handleCommentClick}><PiArrowElbowDownRightBold /></button>
+            {inputVisible && (
+                <div>
+                  <input type="text" placeholder='댓글을 남겨보세요' />
+                  <button>확인</button>
+                </div>
+              )}
+          </li>
         </ul>
       </div>
     </div>
